93272ed7adc74817ee19b1c9d5876eba2beea03b
BoulevardOfBrokenDreams <eplyago@mail.ru>
2022-04-26 11:41:17 +0300

    Добавьте файлы проекта.

---
+++ SecondTry/SecondTry/NewPage.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace SecondTry
+{
+    class NewPage : ContentPage
+    {
+        public NewPage()
+        {
+            Label label = new Label() { Text = "Сколько лет пройдет все о том же гудят провода,  все того же ждут самолеты. Девочка с глазами из самого синего льда тает под огнем пулемета!" +
+                "\n\nЗдесь типо демократия на самом деле царство. я так люблю свою страну и ненавижу государство..." };
+            label.BackgroundColor = Color.Yellow;
+            label.TextColor = Color.Red;
+            label.FontSize = 48;
+            this.Content = label;
+        }
+    }
+}
