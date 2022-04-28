607d56c12f0d689ac7851febeed9adf24bb51efb
BoulevardOfBrokenDreams <eplyago@mail.ru>
2022-04-28 11:50:21 +0300

    Add new page

--- SecondTry/SecondTry/MainPage.xaml.cs
+++ SecondTry/SecondTry/MainPage.xaml.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using Xamarin.Forms;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace SecondTry
 {
@@ -20,8 +20,8 @@ namespace SecondTry
             Button button = (Button)sender;
             button.Text = "Все пиздец";
             button.BackgroundColor = Color.Green;
-            this.BackgroundColor = Color.Blue;
-            lb.Text = "Залупа";
+            BackgroundColor = Color.Blue;
+            label1.Text = "Залупа";
         }
 
         private void OnButton2Clicked(object sender, System.EventArgs e)
@@ -29,15 +29,16 @@ namespace SecondTry
             Button button = (Button)sender;
             button.Text = "ЖМИ";
             button.BackgroundColor = Color.Yellow;
-            this.BackgroundColor = Color.Red;
-            
-        }
-
-        private void Button2Click(object sender, System.EventArgs e)
-        {
+            BackgroundColor = Color.Red;
             button1.Text = "Чмо";
             button2.Text = "Пидр";
-            lb.Text = "писька";
+            label1.Text = "писька";
+        }
+
+        private async void NewPageButtonClick(object sender, System.EventArgs e)
+        {
+            await Navigation.PushModalAsync(new PageFile());
+            
         }
     }
 }
